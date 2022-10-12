b34e53d887f524d2854080284238e5a76e0273fb
DanielFariias <danielfariias15@gmail.com>
2022-10-12 15:47:45 -0300

    project completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
-function App() {
+export default function App() {
+  const [toggleClass, setToggleClass] = useState(false)
+  const [inputEmail, setInputEmail] = useState('')
+
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      )
+  }
+
+  const validEmail = validateEmail(inputEmail)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="form-container">
+      <div className="form-content">
+        <h1>Bem vindo(a)</h1>
+        <form>
+          <label htmlFor="email-input">
+            Email:
+            <input
+              className={
+                inputEmail === ''
+                  ? ''
+                  : validEmail
+                  ? 'valid-input'
+                  : 'invalid-input'
+              }
+              type="text"
+              id="email-input"
+              placeholder="email@email.com"
+              value={inputEmail}
+              onChange={(e) => setInputEmail(e.target.value)}
+            />
+          </label>
+          <button
+            className={toggleClass ? 'position-right' : ''}
+            type="button"
+            onMouseEnter={() => {
+              if (!validEmail) {
+                setToggleClass(!toggleClass)
+              }
+            }}
+            onClick={() => alert('Bem vindo')}
+          >
+            Salvar
+          </button>
+        </form>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
